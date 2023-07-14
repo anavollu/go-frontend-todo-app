c06b8a6284d93162ac961598896b10e6fca9e713
anavollu <anavollu@id.uff.br>
2023-07-14 15:39:08 -0300

    wip: delete, create and update todo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,6 +12,7 @@
 
 	async function createTodo() {
 		let data;
+		let dataList = [];
 		const headersList = {
 			'Content-Type': 'application/json'
 		};
@@ -26,6 +27,9 @@
 
 		if (response.ok) {
 			data = await response.text();
+			const updatedResponse = await fetch('http://localhost:3000/todo?limit=50');
+			dataList = await updatedResponse.json();
+			todos = dataList.todos;
 		} else {
 			console.error(`Error creating data.`);
 		}
@@ -34,6 +38,8 @@
 
 	async function deleteTodo(id: number) {
 		let data;
+		let dataList = [];
+
 		let headersList = {
 			'Content-Type': 'application/json'
 		};
@@ -45,21 +51,47 @@
 
 		if (response.ok) {
 			data = await response.text();
+			const updatedResponse = await fetch('http://localhost:3000/todo?limit=50');
+			dataList = await updatedResponse.json();
+			todos = dataList.todos;
 		} else {
 			console.error(`Error deleting data with ID ${id}.`);
 		}
 	}
+
+	async function updateTodo(id: number) {
+		let data;
+		let headersList = {
+			'Content-Type': 'application/json'
+		};
+
+		let bodyContent = JSON.stringify(formData);
+
+		let response = await fetch(`http://localhost:3000/todo/${id}`, {
+			method: 'PUT',
+			body: bodyContent,
+			headers: headersList
+		});
+
+		if (response.ok) {
+			data = await response.text();
+		} else {
+			console.error(`Error updating data with ID ${id}.`);
+		}
+		console.log(data);
+	}
 </script>
 
-<main>
+<main class="max-w-2xl flex flex-col m-auto">
 	<h1 class="font-bold text-xl text-center">Svelte & Go | To-Do</h1>
 
 	<div class="border border-black p-4">
 		<p class="font-semibold mb-2 text-center">To-Do list</p>
 		<div class="flex flex-col gap-2">
 			{#each todos as todo}
-				<div class="flex justify-between">
-					<p>ID:{todo.ID}, Name:{todo.name}, done:{todo.done}</p>
+				<div class="grid grid-cols-[3fr_2fr_1fr]">
+					<p>Name: {todo.name}</p>
+					<p>Done: {todo.done}</p>
 					<button
 						on:click={() => deleteTodo(todo.ID)}
 						class="bg-red-600 text-white rounded-md p-1 text-sm">Delete</button
